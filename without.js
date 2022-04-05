42f6395fc08e1433214422c375fa5fdb7a01111f
Heron Feijao <heronfeijao@gmail.com>
2022-04-05 19:20:35 -0300

    finished without.js

--- without.js
+++ without.js
@@ -18,3 +18,20 @@ const assertArraysEqual = (arr1, arr2) => {
   }
 };
 
+const without = (arr1, arr2) => {
+  let newArr = [];
+  for (let elem of arr1) {
+    if (!arr2.includes(elem)) {
+      newArr.push(elem);
+    }
+  }
+  return newArr;
+}
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
