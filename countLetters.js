18e60cec947c937b318518007c3dbb9e1970d7a8
Heron Feijao <heronfeijao@gmail.com>
2022-04-06 16:20:27 -0300

    countLetters.js added

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = (string) => {
+  let objLetters = {};
+
+  for (let letter of string) {
+    if (!(letter === " ")) {
+      objLetters[letter] = objLetters[letter] + 1 || 1;
+    }
+  }
+  return objLetters;
+};
+
+console.log(countLetters("lighthouse in the house"));
