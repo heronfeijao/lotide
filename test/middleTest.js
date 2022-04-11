7d4b8603bec374d61e90bc16e83a60706ea6c65c
Heron Feijao <heronfeijao@gmail.com>
2022-04-11 15:38:59 -0300

    updated repo

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,16 +2,6 @@ const assert = require('chai').assert;
 // const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
-// assertArraysEqual([], middle([1])); // => []
-// assertArraysEqual([], middle([1, 2])); // => []
-
-// assertArraysEqual([2], middle([1, 2, 3])); // => [2]
-// assertArraysEqual([3], middle([1, 2, 3, 4, 5])); // => [3]
-
-// assertArraysEqual([2, 3], middle([1, 2, 3, 4])); // => [2, 3]
-// assertArraysEqual([3, 4], middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-
-
 describe("#middle", () => {
 
   it("returns [] for [1])[0]", () => {
